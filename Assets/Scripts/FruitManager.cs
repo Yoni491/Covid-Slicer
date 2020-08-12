30041e5a6ae60be9fa4d56bcf3da2857e7a48a61
Yoni491 <yoniendyoni@gmail.com>
2020-08-12 18:20:48 +0300

    started UI

---
+++ Assets/Scripts/FruitManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform []fruits;
+    float timer = 0;
+    float timeToEject;
+    int randomFruitIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToEject = UnityEngine.Random.Range(2, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>= timeToEject)
+        {
+            timer = 0;
+            timeToEject = UnityEngine.Random.Range(1, 2);
+
+            Transform fruit;
+            fruit = LaunchFruit();
+        }
+
+    }
+
+    private Transform LaunchFruit()
+    {
+        Transform fruit;
+        randomFruitIndex = UnityEngine.Random.Range(0, fruits.Length - 1);
+        fruit = Instantiate(fruits[randomFruitIndex]);
+        fruit.position = transform.position + new Vector3(0, 0, UnityEngine.Random.Range(-1, 1));
+        fruit.GetComponent<Rigidbody>().AddForce(new Vector3(0, 400, 0));
+        return fruit;
+    }
+}
---
+++ Assets/Scripts/FruitScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(1, 0.4f));
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 collisionNormal = (collision.transform.position - transform.position).normalized;
+        float playerCollisionSpeed = Vector3.Dot(collisionNormal, this.GetComponent<Rigidbody>().velocity);
+        if (playerCollisionSpeed < 0) { playerCollisionSpeed = 0; }
+
+        float otherCollisionSpeed = Vector3.Dot(-collisionNormal, collision.rigidbody.velocity);
+        if (otherCollisionSpeed < 0) { otherCollisionSpeed = 0; }
+
+        if (otherCollisionSpeed > playerCollisionSpeed)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
